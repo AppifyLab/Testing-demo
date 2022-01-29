f3d7569617f2f3ecb3f25b418e33b918de641125
nurayni-mou <nuraynimou3s@gmail.com>
2022-01-29 13:38:53 +0600

    done

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -27,9 +27,6 @@ function Login() {
                     </div>
                     <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
                     <div className="_social_login_content">
-                        <div className="_social_login_left_logo _mar_b28">
-                        <img src="assets/images/logo.svg" alt="Image" className="_left_logo" />
-                        </div>
                         <p className="_social_login_content_para _mar_b8">Welcome back</p>
                         <h4 className="_social_login_content_title _titl4 _mar_b50">Login to your account</h4>
                         <button type="button" className="_social_login_content_btn _mar_b40">
